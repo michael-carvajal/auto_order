0276075eda85d039292705fbf50cfabf86429110
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-23 22:44:24 -0400

    got all the way to checkout

--- usa/index.js
+++ usa/index.js
@@ -9,6 +9,36 @@ async function login(username, password, page) {
   await page.getByRole("button").click();
   await page.getByRole("textbox");
 }
+async function searchForItem(page, itemNumber, quantity) {
+  await page.fill("#tirepartField", itemNumber);
+  await page.keyboard.press("Enter");
+  //   const text = await page.getByRole("textbox", { placeholder: "###" }).all();
+  await page.getByText(/Primary Warehouse*/i).click();
+  await page.keyboard.press("Tab");
+  await page.keyboard.press("Tab");
+  await page.keyboard.type(quantity);
+  await page.keyboard.press("Tab");
+  await page.keyboard.press("Enter");
+
+  //   const targetElement = elements[2]; // Index 2 corresponds to the third element
+
+  //   if (targetElement) {
+  //     await targetElement.scrollIntoViewIfNeeded();
+  //     await targetElement.fill('4'); // Example: fill with '4'
+  //   }
+
+  //   text.forEach((t, i) => {
+  //     t.fill(`${i}`);
+  //   });
+  //   console.log(text);
+  //   await page.getByRole("Button", {name : /order now/i})
+  //     await page.click("#global-search-button");
+  //     await page.getByRole("textbox", { name: "Qty" }).fill(quantity);
+  //   //   console.log("we found qty");
+  //     const addToCart = await page.getByText(" Add to cart ").all();
+  //     await addToCart[1].click()
+  //   //   console.log("add to cart");
+}
 
 async function orderFromUSA(
   page,
@@ -30,11 +60,7 @@ async function orderFromUSA(
   // Click on the div containing the store number
   await page.click(`div.divSelectDealerResult >> text=${storeNumber}`);
 
-
-
-  // await page.click("#btn-continue");
-
-  //   await searchForItem(page, itemNumber, quantity);
+  await searchForItem(page, itemNumber, quantity);
 
   // Additional code to handle the search results
   // Example: Extract information from the search results page
