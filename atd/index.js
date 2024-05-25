0f38f24cd123757b33c9ee227f439834c4516b1c
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-05-25 08:56:06 -0400

    able to retrieve confirmation number from atd

--- atd/index.js
+++ atd/index.js
@@ -14,10 +14,10 @@ async function searchForItem(page, itemNumber, quantity) {
   await page.fill("#global-search-input", itemNumber);
   await page.click("#global-search-button");
   await page.getByRole("textbox", { name: "Qty" }).fill(quantity);
-//   console.log("we found qty");
+  //   console.log("we found qty");
   const addToCart = await page.getByText(" Add to cart ").all();
-  await addToCart[1].click()
-//   console.log("add to cart");
+  await addToCart[1].click();
+  //   console.log("add to cart");
 }
 
 async function orderFromATD(
@@ -38,13 +38,20 @@ async function orderFromATD(
     //   console.log(storeId);
     await page.fill("#select-location", storeId);
     await page.click("#btn-continue");
-  
+
     await searchForItem(page, itemNumber, quantity);
-    await page.fill("#customerPO", poNumber)
+    await page.fill("#customerPO", poNumber);
+    // Wait for the confirmation page to load
+    await page.waitForSelector(".order-confirmation-message strong"); // Replace with the actual selector for the confirmation number
+
+    // Extract the confirmation number
+    const confirmationNumber = await page.textContent(
+      ".order-confirmation-message strong"
+    ); // Replace with the actual selector for the confirmation number
+    console.log(confirmationNumber);
   } catch (error) {
     console.log(error);
   }
-
 }
 
 module.exports = orderFromATD;
